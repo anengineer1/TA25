ac055b4e70035facca7266dcfe614456feb6fec8
Francisco Parra <franparpe@disroot.org>
2023-04-17 22:50:59 +0200

    Modified empleados

--- ej2_empleados/src/main/java/com/example/demo/dto/Empleados.java
+++ ej2_empleados/src/main/java/com/example/demo/dto/Empleados.java
@@ -10,121 +10,111 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="empleados")//en caso que la tabla sea diferente
+@Table(name = "empleados") // en caso que la tabla sea diferente
 public class Empleados {
-	
-	//Atributos de entidad Articulos
-		@Id
-		@Column(name = "dni")
-		private String dni;
-		@Column(name = "nombre")
-		private String nombre;
-		@Column(name = "apellidos")
-		private String apellidos;
-		
-		@ManyToOne
-	    @JoinColumn(name = "departamento")
-	    private Departamentos departamento;
 
-		//Constructores
-		/**
-		 * 
-		 */
-		public Empleados() {
-			
-		}
+	// Atributos de entidad Articulos
+	@Id
+	@Column(name = "dni")
+	private String dni;
+	@Column(name = "nombre")
+	private String nombre;
+	@Column(name = "apellidos")
+	private String apellidos;
 
-	
-		/**
-		 * @param id
-		 * @param nombre
-		 * @param apellidos
-		 * @param departamento
-		 */
-		public Empleados(String id, String nombre, String apellidos, Departamentos departamento) {
-			//super();
-			this.dni = id;
-			this.nombre = nombre;
-			this.apellidos = apellidos;
-			this.departamento = departamento;
-		}
+	@ManyToOne
+	@JoinColumn(name = "departamento")
+	private Departamentos departamento;
 
+	// Constructores
+	/**
+	 * 
+	 */
+	public Empleados() {
 
-		/**
-		 * @return the DNI
-		 */
+	}
+
+	/**
+	 * @param id
+	 * @param nombre
+	 * @param apellidos
+	 * @param departamento
+	 */
+	public Empleados(String id, String nombre, String apellidos, Departamentos departamento) {
+		// super();
+		this.dni = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.departamento = departamento;
+	}
+
+	/**
+	 * @return the DNI
+	 */
 //		public String getDNI() {
 //			return dni;
 //		}
 
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
 
-		/**
-		 * @return the nombre
-		 */
-		public String getNombre() {
-			return nombre;
-		}
+	/**
+	 * @return the precio
+	 */
+	public String getApellidos() {
+		return apellidos;
+	}
 
+	/**
+	 * @return the fabricantes
+	 */
+	public Departamentos getDepartamento() {
+		return departamento;
+	}
 
-		/**
-		 * @return the precio
-		 */
-		public String getApellidos() {
-			return apellidos;
-		}
+	/**
+	 * @param codigo the codigo to set
+	 */
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
 
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-		/**
-		 * @return the fabricantes
-		 */
-		public Departamentos getDepartamento() {
-			return departamento;
-		}
+	@Override
+	public String toString() {
+		return "Empleados [dni=" + dni + ", nombre=" + nombre + ", apellidos=" + apellidos + ", departamento="
+				+ departamento + "]";
+	}
 
+	/**
+	 * @return the dni
+	 */
+	public String getDni() {
+		return dni;
+	}
 
-		/**
-		 * @param codigo the codigo to set
-		 */
-		public void setDni(String dni) {
-			this.dni = dni;
-		}
+	/**
+	 * @param apellidos the apellidos to set
+	 */
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
 
-
-		/**
-		 * @param nombre the nombre to set
-		 */
-		public void setNombre(String nombre) {
-			this.nombre = nombre;
-		}
-
-		@Override
-		public String toString() {
-			return "Empleados [dni=" + dni + ", nombre=" + nombre + ", apellidos=" + apellidos + ", departamento="
-					+ departamento + "]";
-		}
-
-
-		/**
-		 * @return the dni
-		 */
-		public String getDni() {
-			return dni;
-		}
-
-
-		/**
-		 * @param apellidos the apellidos to set
-		 */
-		public void setApellidos(String apellidos) {
-			this.apellidos = apellidos;
-		}
-
-
-		/**
-		 * @param departamento the departamento to set
-		 */
-		public void setDepartamento(Departamentos departamento) {
-			this.departamento = departamento;
-		}
+	/**
+	 * @param departamento the departamento to set
+	 */
+	public void setDepartamento(Departamentos departamento) {
+		this.departamento = departamento;
+	}
 
 }
